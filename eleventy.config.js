efc8f6262821fb08620d934d4285d2014aac1b6c
reddsaut <osauter@macalester.edu>
2025-05-29 13:06:07 -0500

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,103 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import path from "node:path";
+import * as sass from "sass";
+
+export default async function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("content/res");
+
+    eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+
+		// opt-out of Eleventy Layouts
+		useLayouts: false,
+
+		compile: async function (inputContent, inputPath) {
+			let parsed = path.parse(inputPath);
+			// Don’t compile file names that start with an underscore
+			if(parsed.name.startsWith("_")) {
+				return;
+			}
+
+			let result = sass.compileString(inputContent, {
+				loadPaths: [
+					parsed.dir || ".",
+					this.config.dir.includes,
+				]
+			});
+
+			// Map dependencies for incremental builds
+			this.addDependencies(inputPath, result.loadedUrls);
+
+			return async (data) => {
+				return result.css;
+			};
+		},
+	});
+
+    eleventyConfig.addTemplateFormats("scss")
+
+    eleventyConfig.addPlugin(feedPlugin, {
+		type: "rss", // or "atom", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "post", // iterate over `collections.posts`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Blog Title",
+			subtitle: "This is a longer description about your blog.",
+			base: "https://example.com/",
+			author: {
+				name: "Your Name",
+				email: "", // Optional
+			}
+		}
+	});
+
+    eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
+
+	function extractExcerpt(post) {
+		if(!post.templateContent) return '';
+		if(post.templateContent.indexOf('</p>') > 0) {
+			let end = post.templateContent.indexOf('</p>');
+			return post.templateContent.substr(0, end+4);
+		}
+		return post.templateContent;
+	}
+
+    eleventyConfig.addCollection("categories", function(collectionApi) {
+        let categories = new Set();
+        let posts = collectionApi.getFilteredByTag('post');
+        posts.forEach(p => {
+            let cats = p.data.categories;
+            cats.forEach(c => categories.add(c));
+        });
+        return Array.from(categories);
+    });
+
+    eleventyConfig.addFilter("filterByCategory", function(posts, cat) {
+		/*
+		case matters, so let's lowercase the desired category, cat
+		and we will lowercase our posts categories
+		*/
+		cat = cat.toLowerCase();
+		let result = posts.filter(p => {
+			let cats = p.data.categories.map(s => s.toLowerCase());
+			return cats.includes(cat);
+		});
+
+		return result;
+	});
+
+    const english = new Intl.DateTimeFormat("en");
+    eleventyConfig.addFilter("niceDate", function(d) {
+	    return english.format(d);
+    });
+    
+    return {
+		dir: {
+			input: 'content'
+		}
+	}
+};
